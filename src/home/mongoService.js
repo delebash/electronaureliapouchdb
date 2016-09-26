fbd3825ab74dce5f339aa387166a8c812588caab
Daniel Elebash <danelebash@hotmail.com>
2016-09-26 15:14:18 -0400

    Yeah it works

--- src/home/mongoService.js
+++ src/home/mongoService.js
@@ -1,17 +1,48 @@
 export default class MongoService {
-  constructor() {
-    this.MongoClient = require('mongodb').MongoClient;
-  }
 
-  testConnection() {
-    return new Promise((resolve, reject) => {
-      this.MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
-        if(!err) {
-          resolve("We are connected");
-        } else {
-          reject(err);
-        }
-      });
-    });
-  }
+    constructor() {
+
+       var NodePouchDB = require('pouchdb');
+       this.leveldbDB = new NodePouchDB('mydb-leveldb');
+    }
+
+    testConnection() {
+        return new Promise((resolve, reject) => {
+            this.leveldbDB.info(function(err, db) {
+                if(!err) {
+                    resolve("We are connected");
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    }
 }
+
+
+
+//
+// export default class MongoService {
+//     constructor() {
+//         this.MongoClient = require('mongodb').MongoClient;
+//       t
+//         this.NodePouchDb =NodePouchDb;
+//         this.MongoClient = require('mongodb').MongoClient;;
+//     }
+//
+//     testConnection() {
+//
+//         // var leveldbDB = new NodePouchDB('mydb-leveldb');
+//
+//         return new Promise((resolve, reject) => {
+//             this.MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+//                 if(!err) {
+//                     resolve("We are connected");
+//                 } else {
+//                     reject(err);
+//                 }
+//             });
+//         });
+//     }
+// }
+
