956ed323d15916ae54b9a3a621d61c495d3479bc
Daniel Elebash <danelebash@hotmail.com>
2016-09-26 08:28:58 -0400

    fixing activate

--- src/home/mongoService.js
+++ src/home/mongoService.js
@@ -1,17 +1,36 @@
-export default class MongoService {
-  constructor() {
-    this.MongoClient = require('mongodb').MongoClient;
-  }
-
-  testConnection() {
-    return new Promise((resolve, reject) => {
-      this.MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
-        if(!err) {
-          resolve("We are connected");
-        } else {
-          reject(err);
-        }
-      });
-    });
-  }
-}
+// export default class MongoService {
+//   constructor() {
+//     this.MongoClient = require('mongodb').MongoClient;
+//   }
+//
+//   testConnection() {
+//     return new Promise((resolve, reject) => {
+//       this.MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+//         if(!err) {
+//           resolve("We are connected");
+//         } else {
+//           reject(err);
+//         }
+//       });
+//     });
+//   }
+// }
+// export default class MongoService {
+//
+//     constructor() {
+//     this.NodePouchDB = require('pouchdb');
+//      this.leveldbDB = new NodePouchDB('mydb-leveldb');
+//   }
+//     testConnection() {
+//         return new Promise((resolve, reject) => {
+//       this.NodePouchDB.info("mongodb://localhost:27017/test", function(err,info) {
+//         if(!err) {
+//           resolve("We are connected");
+//
+//         } else {
+//           reject(err);
+//         }
+//       });
+//     });
+//     };
+// }
